aff438ad3060de1ff19cbbcba11615a05522e7db
chrisx <chrisadriane.ca@gmail.com>
2025-04-28 12:38:56 +0300

    finale

--- src/main/java/com/finsense/service/SmsGateway.java
+++ src/main/java/com/finsense/service/SmsGateway.java
@@ -2,12 +2,5 @@ package com.finsense.service;
 
 public interface SmsGateway {
 
-    /**
-     * Sends an SMS message to a recipient.
-     *
-     * @param recipientPhoneNumber The phone number of the recipient.
-     * @param message The content of the SMS message.
-     * @throws com.finsense.payment.exception.ExternalApiException if the external API call fails.
-     */
     void sendSms(String recipientPhoneNumber, String message);
 }
